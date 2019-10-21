ae5dd3ecb9903e704f5166fe84ca051f0af77e0d
Deazain Putri <55733553+1914370112@users.noreply.github.com>
2019-10-21 02:11:32 -0700

    Add files via upload

---
+++ DEA.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main()
+{
+    printf("\tBIODATA PRIBADI\n\n");
+    printf("NAMA     : DEAZAIN PUTRI IRAWAN\n");
+    printf("NPM      : 1914370112\n");
+    printf("KELAS    : REGULER 1D\n");
+    printf("PRODI    : SISTEM KOMPUTER\n");
+    printf("FAKULTAS :SAINS & TEKNOLOGI\n");
+
+
+    int a=30;
+    int b=20;
+    int c;
+    c=a+b;
+    printf("baris 1 -nilai c adalah %d\n",c);
+    c=a-b;
+    printf("baris 2 -nilai c adalah %d\n",c);
+    c=a*b;
+    printf("baris 3 -nilai c adalah %d\n",c);
+    c=a/b;
+    printf("baris 4 -nilai c adalah %d\n",c);
+    c= b%a;
+    printf("baris 5 -nilai c adalah %d\n",c);
+    return 0;
+}
