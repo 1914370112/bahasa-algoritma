6b124bcdd8db4827a50b99c9346019c1327dcdb9
Deazain Putri <55733553+1914370112@users.noreply.github.com>
2019-10-29 00:04:08 -0700

    Add files via upload

---
+++ dea.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main ()
+{
+printf("                            Nama  :Deazain Putri Irawan\n");
+printf("                            NPM   : 1914370112\n");
+printf("                            penjumlahan,pengurangan,perkalian,pembagian,modulus\n");
+
+int a=5;
+int b =10;
+int c;
+
+
+c=a+b;
+printf("nilai 1 adalah penjumlahan %d\n",c);
+c=a-b;
+printf("nilai 2 adalah pengurangan %d\n",c);
+c=a*b;
+printf("nilai 3 adalah perkalian %d\n",c);
+c=a/b;
+printf("nilai 4 adalah pembagian %d\n",c);
+c=a%b;
+printf("nilai 5 adalah modulus %d\n",c);
+
+return 0;
+}
